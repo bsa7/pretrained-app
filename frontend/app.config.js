bf005c7622d08afdfd6316bc84a38f8bfdc703a2
Sergio Belevskij <bbelevskij@gmail.com>
2022-10-17 00:06:40 +0300

    PP-31 Implement deploy frontend to vps.

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,34 +1,34 @@
 module.exports = {
   expo: {
-    name: "PretrainedApp",
-    slug: "PretrainedApp",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    entryPoint: "./src/app.tsx",
+    name: 'PretrainedApp',
+    slug: 'PretrainedApp',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    entryPoint: './src/app.tsx',
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
     updates: {
-      fallbackToCacheTimeout: 0
+      fallbackToCacheTimeout: 0,
     },
     assetBundlePatterns: [
-      "**/*"
+      '**/*',
     ],
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
-      }
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
     },
     web: {
-      favicon: "./assets/favicon.png"
-    }
-  }
+      favicon: './assets/favicon.png',
+    },
+  },
 }
