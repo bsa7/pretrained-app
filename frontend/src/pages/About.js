d90fc57678e79f3d7840b4f4c536bb19e7e5d665
H-KC <01hkc01@gmail.com>
2022-10-31 21:06:43 +0500

    a small enhancment on the code

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className='main about'>
-      <h3>A web app using pre trained modal</h3>
+      <h3>A web app using pre-trained modal</h3>
       <p>This web app uses pre trained modals to do certain tasks..</p>
       <p>Version: 1.0.1</p>
     </div>
--- frontend/src/pages/Modals.js
+++ frontend/src/pages/Modals.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Objectsdetector from "../components/Modals/Objectsdetector";
 import Texttospeech from "../components/Modals/Texttospeech";
 import Translator from "../components/Modals/Translator";
-
+import holder from "./pretm.svg";
 const Modals = () => {
   // TODO:
   // * create a main layout for the main container
@@ -18,19 +18,34 @@ const Modals = () => {
   return (
     <div className='main modals'>
       <div className='side-bar'>
-        <div className='card' onClick={() => setselectedModal(0)}>
+        <div
+          className={selectedModal === 0 ? "active_card" : ""}
+          onClick={() => setselectedModal(0)}
+        >
           Object-Detection
         </div>
-        <div className='card' onClick={() => setselectedModal(1)}>
+        <div
+          className={selectedModal === 1 ? "active_card" : ""}
+          onClick={() => setselectedModal(1)}
+        >
           Translator
         </div>
-        <div className='card' onClick={() => setselectedModal(2)}>
+        <div
+          className={selectedModal === 2 ? "active_card" : ""}
+          onClick={() => setselectedModal(2)}
+        >
           TextTo-Speech
         </div>
       </div>
 
       <div className='modal-container'>
-        {selectedModal !== null ? getSelectedModal() : <div>empty</div>}
+        {selectedModal !== null ? (
+          getSelectedModal()
+        ) : (
+          <div className='image_holder'>
+            <img src={holder} alt='' />
+          </div>
+        )}
       </div>
     </div>
   );
