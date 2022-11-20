36912ee07e2a0ab70c4263dee4e8f65788577a7b
Sergio Belevskij <bbelevskij@gmail.com>
2022-11-20 10:28:05 +0300

    PP-47 Fix FlaskMVC wrong behaviour

--- api/production_server.py
+++ api/production_server.py
@@ -3,5 +3,5 @@
 from waitress import serve
 from config.application import Application
 
-app = Application(__name__).app()
+app = Application(__name__).app
 serve(app, host = '0.0.0.0', port = '5000', url_scheme = 'https')
