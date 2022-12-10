05f983bfd26d12cfed4087fb953193f54738f31d
Sergio Belevskij <bbelevskij@gmail.com>
2022-12-10 19:43:11 +0300

    PP-30 Работает запись звука на странице

--- api/app/assets/javascript/mentalcounting/index.js
+++ api/app/assets/javascript/mentalcounting/index.js
@@ -1,24 +1,80 @@
 class AudioStream {
-  constructor() {
-    this.microphone = null
+  constructor(triggerButton) {
+    this.chunks = []
+    this.microphone
+    this.mediaRecorder
+    this.stream
+    this.recording = false
+    this.triggerButton = triggerButton
     this.initialize()
   }
 
-  initialize() {
-    this.queryAccessToMicrophone()
+  async initialize() {
+    await this.queryAccessToMicrophone()
+    console.log('initialize#12', { 'this.stream': this.stream })
+    this.initializeMediaRecorder(this.stream)
+    this.triggerButton.addEventListener('click', this.trigger.bind(this))
   }
 
-  queryAccessToMicrophone() {
+  async queryAccessToMicrophone() {
     const audioCtx = new AudioContext();
     if (navigator.mediaDevices) {
-      navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+      await navigator.mediaDevices.getUserMedia({ "audio": true }).then((stream) => {
         this.microphone = audioCtx.createMediaStreamSource(stream);
-        console.log('`microphone` can now act like any other AudioNode')
-      }).catch((err) => {
+        this.stream = stream
+        console.log('microphone can now act like any other AudioNode')
+      }).catch((_err) => {
         console.error('browser unable to access microphone (check to see if microphone is attached)')
       });
     } else {
       console.log('browser unable to access media devices (update your browser)')
     }
+  }
+
+  initializeMediaRecorder(stream) {
+    // Instantiate the media recorder.
+    this.mediaRecorder = new MediaRecorder(stream);
+    // Create a buffer to store the incoming data.
+    this.chunks = []
+    this.mediaRecorder.ondataavailable = this.mediaRecorderOnDataAvailable.bind(this)
+    this.mediaRecorder.onstop = this.mediaRecorderOnStop.bind(this)
+  }
+
+  startRecording() {
+    this.recording = true
+    this.triggerButton.innerText = 'Stop Recording'
+    this.mediaRecorder.start()
+  }
+
+  stopRecording() {
+    this.recording = false
+    this.triggerButton.innerText = 'Start Recording'
+    this.mediaRecorder.stop()
+  }
+
+  trigger(_event) {
+    if (this.recording) {
+      this.stopRecording()
+    } else {
+      this.startRecording()
+    }
+  }
+
+  mediaRecorderOnDataAvailable(event) {
+    console.log('ondataavailable#34', { 'event.data': event.data })
+    this.chunks.push(event.data);
+  }
+
+  mediaRecorderOnStop(event) {
+    console.log('mediaRecorderOnStop#45', { event })
+    // A "blob" combines all the audio chunks into a single entity
+    const audio = new Audio();
+    audio.setAttribute("controls", "");
+    document.querySelector('#sound-clip').appendChild(audio)
+
+    // Combine the audio chunks into a blob, then point the empty audio clip to that blob.
+    const blob = new Blob(this.chunks, {"type": "audio/ogg; codecs=opus"});
+    audio.src = window.URL.createObjectURL(blob);
+    this.chunks = []; // clear buffer
   }
 }
