ec303a5d18e7c82105e47c4115f8dd292a73f627
Vicheslav Makeev <Viacheslav.Makeev@urfu.me>
2022-12-24 14:39:35 +0500

    PP-27 автореферирование текста

---
+++ api/tests/controllers/summarization_controller/index_page_test.py
@@ -0,0 +1,26 @@
+''' This is test case for summarization page '''
+
+from flask import url_for
+from ward import each, test
+from tests.fixtures import client, browser
+import pdb
+
+@test('Возвращает статус 200 for GET запроса', tags=['request', 'text_summarization'])
+def _(client = client, resource = each('summarization')):
+  response = client.get(url_for(f'{resource}.index'))
+  assert response.status_code == 200
+
+@test('На странице /summarization отображается надпись "Автореферат текста онлайн"', tags=['request', 'text_summarization'])
+def _(client = client, resource = each('summarization')):
+  response = client.get(url_for(f'{resource}.index'))
+  assert 'Автореферат текста онлайн' in response.text
+
+@test('На странице /summarization отображается кнопка submit"', tags=['request', 'text_summarization'])
+def _(client = client, resource = each('summarization')):
+  response = client.get(url_for(f'{resource}.index'))
+  assert 'value = "Выполнить автореферирование"' in response.text
+
+@test('На странице /summarization находится поле ввода для адреса изображения', tags=['request', 'text_summarization', 'current'])
+def _(client=client, resource=each('summarization')):
+  response = client.get(url_for(f'{resource}.index'))
+  assert '<textarea' in response.text
