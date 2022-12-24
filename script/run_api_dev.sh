83b7a36a89f7417b24cee0f74c8016890a4f608b
Vicheslav Makeev <Viacheslav.Makeev@urfu.me>
2022-12-24 10:32:44 +0500

    tmp

--- script/run_api_dev.sh
+++ script/run_api_dev.sh
@@ -2,5 +2,5 @@
 
 echo 'Run Flask app'
 mkdir -p ./api/tmp/models
-wget https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth -O ./api/tmp/models/ColorizeArtistic_gen.pth
+#wget https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth -O ./api/tmp/models/ColorizeArtistic_gen.pth
 cd ./api && API_ENV=development python development_server.py
